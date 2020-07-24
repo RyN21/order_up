4bac48650f6b855e86649968d996acdcf54cd879
ryn21 <33795357+RyN21@users.noreply.github.com>
2020-07-24 10:36:53 -0600

    Add spec for extension

--- spec/features/chefs/show_spec.rb
+++ spec/features/chefs/show_spec.rb
@@ -5,12 +5,19 @@ RSpec.describe "Chef's Show Page" do
     @chef = Chef.create!(name: "Frank")
     @pizza = @chef.dishes.create!(name: "Pizza", description: "Thick crust and extra cheesy")
     @bread_sticks = @chef.dishes.create!(name: "Bread Sticks", description: "Tasty seasoned bread sticks")
+    @calzone = @chef.dishes.create!(name: "Bread Sticks", description: "Tasty seasoned bread sticks")
+    @cheezy_bread = @chef.dishes.create!(name: "Bread Sticks", description: "Tasty seasoned bread sticks")
     @bread = Ingredient.create!(name: "Bread", calories: 200)
     @cheese = Ingredient.create!(name: "Cheese", calories: 400)
-    @sause = Ingredient.create!(name: "Marinara Sauase", calories: 150)
+    @sause = Ingredient.create!(name: "Marinara Sause", calories: 150)
     DishIngredient.create!(dish: @pizza, ingredient: @bread)
     DishIngredient.create!(dish: @pizza, ingredient: @cheese)
     DishIngredient.create!(dish: @pizza, ingredient: @sause)
+    DishIngredient.create!(dish: @calzone, ingredient: @bread)
+    DishIngredient.create!(dish: @calzone, ingredient: @cheese)
+    DishIngredient.create!(dish: @calzone, ingredient: @sause)
+    DishIngredient.create!(dish: @cheezy_bread, ingredient: @bread)
+    DishIngredient.create!(dish: @cheezy_bread, ingredient: @cheese)
     DishIngredient.create!(dish: @bread_sticks, ingredient: @bread)
   end
 
@@ -37,5 +44,13 @@ RSpec.describe "Chef's Show Page" do
       expect(page).to have_content(@bread.name)
       expect(page).to have_content(@sause.name)
     end
+  end
+
+  it "displays the chef's 3 most used ingredients" do
+    visit "/chefs/#{@chef.id}"
+
+    expect(page).to have_content("1. Bread")
+    expect(page).to have_content("2. Cheese")
+    expect(page).to have_content("3. Marinara Sause")
   end
 end
